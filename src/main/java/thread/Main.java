72737d3d23b7750d61f725a3fef5fcdd2b877924
EvgeniyM <smilevich@yandex.ru>
2025-12-15 04:11:42 +0300

    Обновил Main

--- src/main/java/thread/Main.java
+++ src/main/java/thread/Main.java
@@ -1,17 +1,44 @@
 package thread;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.concurrent.Semaphore;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    private static final Semaphore evenSemaphore = new Semaphore(1);
+    private static final Semaphore oddSemaphore = new Semaphore(0);
+
+    public static void main(String[] args) {
+        Thread evenThread = new Thread(() -> {
+            for (int i = 0; i <= 10; i += 2) {
+                try {
+                    evenSemaphore.acquire();
+                    System.out.println("Четный " + i);
+                    oddSemaphore.release();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        Thread oddThread = new Thread(() -> {
+            for (int i = 1; i <= 9; i += 2) {
+                try {
+                    oddSemaphore.acquire();
+                    System.out.println("Нечетный " + i);
+                    evenSemaphore.release();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
+        evenThread.start();
+        oddThread.start();
+
+        try {
+            evenThread.join();
+            oddThread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
